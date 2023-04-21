b7d2e291527b3bf556b51a3dba7864bfab16c6ba
Xevione <117281564+Xevione@users.noreply.github.com>
2023-04-21 13:15:57 +0200

    Add files via upload
    Upload des collisions balles + mort des ennemies

---
+++ MiniStudio-1/game.py
@@ -0,0 +1,119 @@
+# Example file showing a circle moving on screen
+import pygame
+import math
+from player import Player,PlayerBullet
+from enemies import Enemy
+
+# Creating a gameState class for game info
+class gameState ():
+    def __init__(self, Map, currentScrollDirection = "Right"):
+        self.Map = Map
+        self.currentScrollDirection = currentScrollDirection
+
+# pygame setup
+pygame.init()
+#creating a screen
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+
+thisPlayer=Player(currentSurface=screen, visualsList=["image/astronaute_1.png","image/astronaute_2.png"], position = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
+walkCycle = 0
+
+#preparing the scrolling screen
+bg = pygame.transform.scale(pygame.image.load("image/background2.jpg"),(1005,screen.get_height()))
+bg_width = bg.get_width()
+scroll = 0
+tiles = math.ceil(screen.get_width() / bg_width) +1
+
+#launching the game
+running = True
+dt = 0
+
+#preparing shown enemy storage list
+enemiesOnScreen = []
+
+while running:
+
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # draw scrolling background
+    for i in range(0, tiles):
+        screen.blit(bg,(i*bg_width+ scroll,0))
+    scroll -= 5
+    #scroll reset
+    if abs(scroll) > bg_width:
+        scroll = 0
+    
+    # animates the player in the right place for every frame
+    thisPlayer.animate()
+    for i in enemiesOnScreen:
+        i.ai()
+
+    # decreases the cooldown on the player's attack
+    thisPlayer.currentShotCoolDown -=1
+
+
+    if thisPlayer.shotsList:
+        toDelete=[]
+        for i in range(len(thisPlayer.shotsList)):
+            keeping = thisPlayer.shotsList[i].move(dt)
+            if not keeping:
+                toDelete.append(i)
+        if toDelete:
+            for i in toDelete:
+                del thisPlayer.shotsList[i]
+    
+    
+    
+    
+    keys = pygame.key.get_pressed()
+    if (keys[pygame.K_z] or keys[pygame.K_UP]) and thisPlayer.position.y > 0:
+        thisPlayer.position.y -= 400 * dt
+    if (keys[pygame.K_s] or keys[pygame.K_DOWN]) and thisPlayer.position.y < screen.get_height() - 80:
+        thisPlayer.position.y += 400 * dt
+    if (keys[pygame.K_q] or keys[pygame.K_LEFT]) and thisPlayer.position.x > 0:
+        thisPlayer.position.x -= 400 * dt
+    if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and thisPlayer.position.x < screen.get_width() - 80:
+        thisPlayer.position.x += 400 * dt
+    if keys[pygame.K_SPACE]:
+        thisPlayer.shoot()
+    # testing enemy creation
+    if keys[pygame.K_a]:
+        print("a pressed")
+        enemiesOnScreen.append(Enemy(screen).spawn())
+
+     #Collision    
+    for i in range (len(enemiesOnScreen)):
+            if thisPlayer.shotsList:
+                for a in range (len(thisPlayer.shotsList)):
+                    collision = PlayerBullet.isCollision(thisPlayer.shotsList[a],enemiesOnScreen[i].position,80)
+                    if collision :
+                        thisPlayer.shotsList[a].position.y = 730
+                        enemiesOnScreen[i].hp -= 1
+                        print(enemiesOnScreen[i].hp)
+
+
+    if enemiesOnScreen:
+        DelEnemies = []
+        for i in range(len(enemiesOnScreen)-1,-1,-1):
+            if (enemiesOnScreen[i].die()==True):
+                print(enemiesOnScreen[i].die())
+                DelEnemies.append(i)
+                print("a")
+                if DelEnemies:
+                    del enemiesOnScreen[i]
+                    print("supprimer")
+
+    pygame.display.update()
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
---
+++ MiniStudio-1/player.py
@@ -0,0 +1,64 @@
+import pygame
+import math
+
+class Player ():
+    def __init__(self, currentSurface, visualsList = [],  currentVisualsFrame = 0, visualsDelayer = 0, shotVisuals = "shot.png",damage = 1,shotSpeed = 2, shotCoolDown = 20, currentShotCoolDown = 0, shotsList = [] , position = pygame.Vector2(0,0) ,lives = 3):
+        # general data
+        self.currentSurface = currentSurface # which surface the player is currently seen
+        self.position = position # the player's position on screen
+        self.lives = lives # number of lives the player has
+
+        # visuals data
+        self.visualsList = visualsList # list of paths to the different frames of the character's animation
+        self.currentVisualsFrame =  currentVisualsFrame # which animation frame is currently displayed
+        self.visualsDelayer = visualsDelayer # counts the delay between animation frames
+
+        # bullet data
+        self.shotVisuals = shotVisuals # what the player's attacks look like
+        self.damage = damage # the current damage dealt by the player's attacks
+        self.shotSpeed = shotSpeed # the speed at which the player's attacks will travel
+        self.shotCoolDown = shotCoolDown # total delay between each attack
+        self.currentShotCoolDown = currentShotCoolDown # decreases over time, counts time before next attack
+        self.shotsList = shotsList # stores every bullet fired by the player
+
+
+    def shoot (self):
+        if self.currentShotCoolDown < 1:
+            self.shotsList.append(PlayerBullet(self.currentSurface, self.damage, self.shotSpeed, pygame.Vector2(self.position.x+80,self.position.y+40)))
+            self.currentShotCoolDown = 0 + self.shotCoolDown
+    
+    def animate (self):
+        self.visualsDelayer +=1
+        if self.visualsDelayer == 14:
+                self.visualsDelayer = 0
+                if self.currentVisualsFrame == len(self.visualsList) - 1:
+                    self.currentVisualsFrame = 0
+                else :
+                    self.currentVisualsFrame +=1
+        self.currentSurface.blit(pygame.transform.scale(pygame.image.load(self.visualsList[self.currentVisualsFrame]),(80,80)), self.position)
+
+class PlayerBullet ():
+    def __init__(self, currentSurface, dmg, spd, position = pygame.Vector2(0,0)):
+        self.currentSurface = currentSurface
+        self.dmg = dmg
+        self.spd = spd
+        self.position = position
+
+    #making the visible bullets move every frame
+    def move(self, dt):
+        if self.currentSurface.get_width() + 40 > self.position.x:
+            pygame.draw.circle(self.currentSurface, "green", self.position, 10)
+            self.position.x += 300 * dt * self.spd
+            return True
+        else:
+            return False
+        
+    def isCollision (self,Object:pygame.Vector2,size):
+        #distance = math.sqrt(math.pow(Object-self.position.x,2)+(math.pow(ObjectY-self.position.y,2)))
+        vect = Object + pygame.Vector2(size/2,size/2)
+        distance = (vect-self.position).magnitude()
+        if distance < size/2:
+            return True
+        else:
+            return False
+
